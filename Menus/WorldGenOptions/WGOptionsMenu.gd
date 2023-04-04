094d6db73019564b0585186543db22e41928d813
Axis <axis@Axiss-MacBook-Pro.local>
2023-04-04 14:20:09 -0700

    Added new shit

--- Menus/WorldGenOptions/WGOptionsMenu.gd
+++ Menus/WorldGenOptions/WGOptionsMenu.gd
@@ -1,20 +1,22 @@
 extends Control
 
-@onready var worldsize_option = $Panel/OptionButton
-
-#var worldgen = get_node()
-#preload("res://Scenes/World Generation.tscn")
-#get_node("other_node_with_script).yourfunction() or get_node("other_node_with_script).yourvar
-#var worldgeninstance = worldgen.instantiate()
+@onready var worldtype_option = $Panel/world_type
+@onready var worldsize_option = $Panel/world_size
 
 func _ready():
 	add_item()
 
 func add_item():
+	#World size options
 	worldsize_option.add_item("Small")
 	worldsize_option.add_item("Medium")
 	worldsize_option.add_item("Large")
+	
+	#World type options
+	worldtype_option.add_item("Nero")
+	worldtype_option.add_item("Gaia-1-247")
 
+#World Size
 func _on_OptionButton_item_selected(index):
 	var current_selected = index
 	
@@ -25,6 +27,16 @@ func _on_OptionButton_item_selected(index):
 	elif current_selected == 2:
 		Global.world_size = "large"
 
+#World Type
+func _on_world_type_item_selected(index):
+	var current_selected = index
+	
+	if current_selected == 0:
+		Global.world_type= "Nero"
+	elif current_selected == 1:
+		Global.world_type = "Gaia"
+
 
 func _on_button_pressed():
+	Global.new_game = true
 	get_tree().change_scene_to_file("res://Scenes/World Generation.tscn")
--- global.gd
+++ global.gd
@@ -1,13 +1,76 @@
 extends Node
 
+var new_game = false
+
+#World Generation Options
+var world_type
 var world_size
 
+#Facilities
+#Productions
+var provision = []
+var SPP = []
+var LSS = []
 
-# Called when the node enters the scene tree for the first time.
+#Hospitality
+var residences = []
+var medical = []
+
+#Military
+var armoury = []
+var hangar = []
+
+#Development
+var research = []
+
+#Counters
+var energy_max
+var energy_current
+var energy_rate
+
+var food_max
+var food_current
+var food_rate
+
+var oxygen_max
+var oxygen_current
+var oxygen_rate
+
 func _ready():
-	pass # Replace with function body.
-
+	#If new game, add facilities to variables
+	if new_game:
+		provision.append({
+			id = provision.size(),
+			tier = 1
+		})
+		SPP.append({
+			id = SPP.size(),
+			tier = 1
+		})
+		LSS.append({
+			id = LSS.size(),
+			tier = 1
+		})
+		residences.append({
+			id = residences.size(),
+			tier = 1
+		})
+		medical.append({
+			id = medical.size(),
+			tier = 1
+		})
+		armoury.append({
+			id = armoury.size(),
+			tier = 1
+		})
+		hangar.append({
+			id = hangar.size(),
+			tier = 1
+		})
+		research.append({
+			id = research.size(),
+			tier = 1
+		})
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
