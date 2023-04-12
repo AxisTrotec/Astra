f98377ebe78b9274f41c0a1f5113c183b3e11304
Axis <axis@Axiss-MacBook-Pro.local>
2023-04-12 08:35:29 -0700

    New

--- Scenes/World Generation/Camera2D.gd
+++ Scenes/World Generation/Camera2D.gd
@@ -4,14 +4,19 @@ extends Camera2D
 @onready var cur_oxygen = $"Counter Bar/CurrentOxygen"
 @onready var cur_food = $"Counter Bar/CurrentFood"
 
+#Panels
 @onready var panel = $"Counter Bar"
+@onready var FacilitiesPanel = $"Facilities Panel"
 
 func _ready():
-	print(DisplayServer.screen_get_dpi())
+	#Set panels on top-left corner of the screen
 	panel.set_position(Vector2(-18000,-10500))
-	panel.scale.x = 40
-	panel.scale.y = 40
-	print(panel.get_screen_position())
+	panel.scale.x = 20
+	panel.scale.y = 20
+	
+	FacilitiesPanel.set_position(Vector2(-18000,-10500))
+	FacilitiesPanel.scale.x = 20
+	FacilitiesPanel.scale.y = 20
 	
 	#Set counter variables
 	cur_energy.text = str(Global.energy_current)
@@ -21,29 +26,18 @@ func _ready():
 	#Timer for counter updates with rates
 	var timer := Timer.new()
 	add_child(timer)
-	timer.wait_time = 10
+	
+	#60 seconds
+	timer.wait_time = 60
 	timer.start()
 	timer.timeout.connect(_on_timer_timeout)
 	
-		
 func _on_timer_timeout() -> void:
+	#Energy Rate
 	Global.energy_current += 13
 	cur_energy.text = str(Global.energy_current)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _input(event):
-#	var velocity = Vector2()
-#
-#	if abs(zoompos.x - get_global_mouse_position().x) > zoomMargin:
-#		zoomfactor = 1.0
-#	if abs(zoompos.y - get_global_mouse_position().x) > zoomMargin:
-#		zoomfactor = 1.0
-#
 	if event is InputEventKey:
 		if event.is_pressed():
 			#Movements
